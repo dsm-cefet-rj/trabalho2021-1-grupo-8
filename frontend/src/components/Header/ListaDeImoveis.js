450f680e13608c37ccd3b92a79c24cba9a5fc7f7
Pedro Lucas <ppedrolangelo@gmail.com>
2021-08-10 17:52:57 -0300

    imagens dos imoveis agora sao links

--- frontend/src/components/Header/ListaDeImoveis.js
+++ frontend/src/components/Header/ListaDeImoveis.js
@@ -5,27 +5,33 @@ export class ListaDeImoveis extends Component {
   render() {
     return (
       <div>
-        <a>
           <ul>
-            <li className="lista">
-              <img className="img" src="Apartamento1.jpg" />
-              <h2>R$ 1000/mês</h2>
-              <p>Descrição da casa</p>
-            </li>
+            <a href="">
+              <li className="lista">
+                <img className="img" src="Apartamento1.jpg" />
+                <h2>R$ 1000/mês</h2>
+                <p>Descrição da casa</p>
+              </li>
+            </a>
             <br />
+
+            <a href="">
             <li className="lista">
               <img className="img" src="apartamento2.jpg" />
               <h2>R$ 900/mês</h2>
               <p>Descrição da casa</p>
             </li>
+            </a>
             <br />
+
+            <a href="">
             <li className="lista">
               <img className="img" src="apartamento3.jpg" />
               <h2>R$ 2000/mês</h2>
               <p>Descrição da casa</p>
             </li>
+            </a>
           </ul>
-        </a>
       </div>
     );
   }
