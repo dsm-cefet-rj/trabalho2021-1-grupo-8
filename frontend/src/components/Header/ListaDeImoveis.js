c10e12f738db8b3814ff25d6c22a3cef0a8b5086
Pedro Lucas <ppedrolangelo@gmail.com>
2021-08-10 17:48:53 -0300

    componente de imoveis foi estilizado

--- frontend/src/components/Header/ListaDeImoveis.js
+++ frontend/src/components/Header/ListaDeImoveis.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./imoveis.css"
 
 export class ListaDeImoveis extends Component {
   render() {
@@ -6,20 +7,20 @@ export class ListaDeImoveis extends Component {
       <div>
         <a>
           <ul>
-            <li ul class="container">
-              <img id="apartamento1" src="Apartamento1.jpg" />
+            <li className="lista">
+              <img className="img" src="Apartamento1.jpg" />
               <h2>R$ 1000/mês</h2>
               <p>Descrição da casa</p>
             </li>
             <br />
-            <li class="container">
-              <img id="apartamento2" src="apartamento2.jpg" />
+            <li className="lista">
+              <img className="img" src="apartamento2.jpg" />
               <h2>R$ 900/mês</h2>
               <p>Descrição da casa</p>
             </li>
             <br />
-            <li class="container">
-              <img id="apartamento3" src="apartamento3.jpg" />
+            <li className="lista">
+              <img className="img" src="apartamento3.jpg" />
               <h2>R$ 2000/mês</h2>
               <p>Descrição da casa</p>
             </li>
