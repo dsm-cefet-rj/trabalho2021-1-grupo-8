3d587f269d8f16d02dd68632753f140bc8f71441
macpedro12 <pedroamorim62@gmail.com>
2021-08-25 18:17:51 -0300

    Adicionado api ao chat

--- frontend/src/components/Chat/chat.js
+++ frontend/src/components/Chat/chat.js
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./chat.module.css";
 
 export default function Chat() {
+  const [values, setValues] = useState({});
+
+  function onChange(event) {
+    const { name, value } = event.target;
+
+    setValues({ ...values, [name]: value });
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    axios.post("http://localhost:5000/chat", values);
+  }
+  const [chat, setChat] = useState([])
+  useEffect(()=>{
+    
+    axios.get("http://localhost:5000/chat").then((response)=>{
+      setChat(response.data);
+    })
+  },[])
+  function resize(){
+    window.location.reload();
+} 
   return (
     <div className={styles.all}>
       <div className={styles.people}>
@@ -12,9 +37,18 @@ export default function Chat() {
         <div className={styles.name}>
           <p>Nome</p>
         </div>
+        <div className={styles.chat2}>
+         {chat.map((c)=>(
+           <div className={styles.msg}>
+             <span>{c.chat}</span>
+            </div>
+         ))}
+        </div>
         <div className={styles.send}>
-          <input />
-          <button>Enviar</button>
+          <form onSubmit={onSubmit}>
+            <input name="chat" onChange={onChange}  />
+            <button onClick={resize} >Enviar</button>
+          </form>
         </div>
       </div>
     </div>
