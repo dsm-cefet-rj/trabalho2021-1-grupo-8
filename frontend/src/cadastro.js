bf90e854fdfba2364cb9206b7401106b95b6b662
Pedro Lucas <ppedrolangelo@gmail.com>
2021-08-25 01:18:50 -0300

    pagina de cadastro salva no json server

--- frontend/src/cadastro.js
+++ frontend/src/cadastro.js
@@ -1,15 +1,39 @@
 // import "./imoveis.css";
+import { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from  'react-router-dom'
 
-export default function cadastro() {
+
+
+export default function Cadastro() {
+  const [values, setValues] = useState({ });
+  const history = useHistory();
+
+  function onChange(event){
+    const { name, value } = event.target;
+
+    setValues({ ...values, [name]: value })
+  }
+
+  function onSubmit(event){
+    event.preventDefault();
+
+    axios.post('http://localhost:5000/login', values)
+    .then((response) => {
+      history.push('/imoveis');
+    });
+  }
+
     return (
-       <form>
-        <label>Email</label>
-        <input/>
-
-        <br></br>
-        <label>Senha</label>
-        <input/>
-      </form>
+      <div>
+        <form onSubmit={onSubmit}>
+          <label>Email</label>
+          <input name="email" onChange={onChange}/>
+          <label>Senha</label>
+          <input name="senha" onChange={onChange}/>
+          <button type="submit">Cadastrar</button>
+        </form>
+      </div>
     );
   }
   
